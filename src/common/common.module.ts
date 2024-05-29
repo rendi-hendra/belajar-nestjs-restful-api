31be5e79de60e8230ee28a285a8b1289faeeedd3
babe25 <babehndr5@gmail.com>
2024-05-30 02:41:56 +0700

    Replace app module with common module

---
+++ src/common/common.module.ts
@@ -0,0 +1,22 @@
+import { Global, Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { WinstonModule } from 'nest-winston';
+import * as winston from 'winston';
+import { PrismaService } from './prisma.service';
+import { ValidationService } from './validation.service';
+
+@Global()
+@Module({
+  imports: [
+    WinstonModule.forRoot({
+      format: winston.format.json(),
+      transports: [new winston.transports.Console()],
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
+  providers: [PrismaService, ValidationService],
+  exports: [PrismaService, ValidationService],
+})
+export class CommonModule {}
