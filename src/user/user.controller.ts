3291cfc7e5ef7476735d939bbddbdd913c4b81b2
babe25 <babehndr5@gmail.com>
2024-06-02 01:14:49 +0700

    Add user login functionality and associated tests

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,10 @@
 import { Body, Controller, HttpCode, Post } from '@nestjs/common';
 import { UserService } from './user.service';
-import { RegisterUserRequest, UserResponse } from '../model/user.model';
+import {
+  LoginUserRequest,
+  RegisterUserRequest,
+  UserResponse,
+} from '../model/user.model';
 import { WebResponse } from '../model/web.model';
 
 @Controller('/api/users')
@@ -13,6 +17,17 @@ export class UserController {
     @Body() request: RegisterUserRequest,
   ): Promise<WebResponse<UserResponse>> {
     const result = await this.userService.register(request);
+    return {
+      data: result,
+    };
+  }
+
+  @Post('/login')
+  @HttpCode(200)
+  async login(
+    @Body() request: LoginUserRequest,
+  ): Promise<WebResponse<UserResponse>> {
+    const result = await this.userService.login(request);
     return {
       data: result,
     };
--- test/user.spec.ts
+++ test/user.spec.ts
@@ -26,7 +26,7 @@ describe('UserController', () => {
 
   describe('POST /api/users', () => {
     beforeEach(async () => {
-      testService.deleteUser();
+      await testService.deleteUser();
     });
     it('should be rejected if request is invalid', async () => {
       const response = await request(app.getHttpServer())
@@ -60,6 +60,7 @@ describe('UserController', () => {
     expect(response.body.data.name).toBe('test');
   });
   it('should be rejected if username already exists', async () => {
+    await testService.deleteUser();
     await testService.createUser();
     const response = await request(app.getHttpServer())
       .post('/api/users')
@@ -73,5 +74,43 @@ describe('UserController', () => {
 
     expect(response.status).toBe(400);
     expect(response.body.errors).toBeDefined();
+  });
+
+  // New
+
+  describe('POST /api/users/login', () => {
+    beforeEach(async () => {
+      await testService.deleteUser();
+      await testService.createUser();
+    });
+    it('should be rejected if request is invalid', async () => {
+      const response = await request(app.getHttpServer())
+        .post('/api/users/login')
+        .send({
+          username: '',
+          password: '',
+        });
+
+      logger.info(response.body);
+
+      expect(response.status).toBe(400);
+      expect(response.body.errors).toBeDefined();
+    });
+  });
+
+  it('should be able to login', async () => {
+    const response = await request(app.getHttpServer())
+      .post('/api/users/login')
+      .send({
+        username: 'test',
+        password: 'test',
+      });
+
+    logger.info(response.body);
+
+    expect(response.status).toBe(200);
+    expect(response.body.data.username).toBe('test');
+    expect(response.body.data.name).toBe('test');
+    expect(response.body.data.token).toBeDefined();
   });
 });
